1704ee4cb5302739b053b6be936ae0f2f318a92b
reeds-git <freefreefree2013@gmail.com>
2016-09-22 15:00:33 -0600

    added more files

---
+++ project1/KNNClassifier.py
@@ -0,0 +1,10 @@
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split as split
+
+import LoadFile
+
+
+class KNN:
+    def knn(self, K, data, dataClass, inputs):
+        return 3
+
